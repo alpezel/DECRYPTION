7904f7fb5a8d1b78c480eeacb60b9125e226ac3f
alpezel <allistairevillaruel@gmail.com>
2023-04-21 16:23:14 +0800

    pseudo code

---
+++ decryption.py
@@ -0,0 +1,10 @@
+# ask user for the input
+
+# check the character using loop
+# if *, change to "a"
+# if &, change to  "e"
+# if #, change to "i"
+# if +, change to "o"
+# !, change to "u"
+
+# print output
