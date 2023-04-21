e13f587811805373088f99db04af558a12da8b86
alpezel <allistairevillaruel@gmail.com>
2023-04-21 17:34:48 +0800

    decrypting using for loop
    test " * " in the input and change to "a"

--- decryption.py
+++ decryption.py
@@ -2,10 +2,16 @@
 input_string = input("What's the encrypted text you want to decrypt?")
 output_string = ""
 # check the character using loop
-# if *, change to "a"
-# if &, change to  "e"
-# if #, change to "i"
-# if +, change to "o"
-# !, change to "u"
+for i in range(len(input_string)):
+    # if *, change to "a"
+    if input_string[i] == "*":
+        output_string += "a"
+    # if &, change to  "e"
+    # if #, change to "i"
+    # if +, change to "o"
+    # !, change to "u"
+    else:
+        output_string += input_string[i]
 
 # print output
+print("\nThe decrypted text is: ","\033[4m"+"\033[1;33m"+ output_string)
